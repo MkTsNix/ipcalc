05b01ab84e83a6aae6ce419ec81d4ab2346bdf54
MkTsNix <mihail.tsay@gmail.com>
2021-02-18 16:48:30 +0600

    change color

--- ip_calc.py
+++ ip_calc.py
@@ -3,7 +3,7 @@ import ipaddress as ip
 
 class bcolors:
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
+    WARNING = '\033[93m'
 
 net = ip.IPv4Network(sys.argv[1], False)
 
@@ -16,8 +16,8 @@ print(
     f'|-----------------------------------------------------------|\n'
     f'|     Network              |     {net}            |\n'
     f'|     Netmask              |     {net.netmask}              |\n'
-    f'|     {bcolors.BOLD}First host           |     {host[0]}{bcolors.ENDC}               |\n'
-    f'|     {bcolors.BOLD}Last host            |     {host[-1]}{bcolors.ENDC}             |\n'
+    f'|     {bcolors.WARNING}First host           |     {host[0]}{bcolors.ENDC}               |\n'
+    f'|     {bcolors.WARNING}Last host            |     {host[-1]}{bcolors.ENDC}             |\n'
     f'|     Broadcast            |     {net.broadcast_address}             |    \n'
     f'|  Quantity avalible hosts |     {net.num_addresses - 2}                        |\n'
     f'|     Public               |     {isGlobal}                      |\n'
