4b8b0aeece0dbb319b5f1862ceb1c47815b09e60
MkTsNix <mihail.tsay@gmail.com>
2021-02-18 17:29:02 +0600

    Add exception for cli input

--- ip_calc.py
+++ ip_calc.py
@@ -3,10 +3,24 @@ import sys
 import ipaddress as ip
 
 class bcolors:
-    ENDC = '\033[0m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
     WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+if len(sys.argv) < 2:
+    print(f'{bcolors.FAIL}ERROR: Please input network with prefix{bcolors.ENDC}\n'
+          f'{bcolors.OKBLUE}Example: ./ip_calc.py 192.168.0.0/24 or python3 ip_calc.py 192.168.0.0/24{bcolors.ENDC}')
+    sys.exit(1)
+
 
 net = ip.IPv4Network(sys.argv[1], False)
+
 
 host = list(net.hosts())
 isGlobal = False
@@ -15,12 +29,12 @@ if net.is_global == True:
 
 print(
     f'|-----------------------------------------------------------|\n'
-    f'|     Network              |     {net}            |\n'
-    f'|     Netmask              |     {net.netmask}              |\n'
-    f'|     {bcolors.WARNING}First host           |     {host[0]}{bcolors.ENDC}               |\n'
-    f'|     {bcolors.WARNING}Last host            |     {host[-1]}{bcolors.ENDC}             |\n'
-    f'|     Broadcast            |     {net.broadcast_address}             |    \n'
-    f'|  Quantity avalible hosts |     {net.num_addresses - 2}                        |\n'
-    f'|     Public               |     {isGlobal}                      |\n'
+    f'     Network              |     {net}\n'
+    f'     Netmask              |     {net.netmask}\n'
+    f'     {bcolors.WARNING}First host{bcolors.ENDC}           |     {bcolors.WARNING}{host[0]}{bcolors.ENDC}\n'
+    f'     {bcolors.WARNING}Last host{bcolors.ENDC}            |     {bcolors.WARNING}{host[-1]}{bcolors.ENDC}\n'
+    f'     Broadcast            |     {net.broadcast_address}\n'
+    f'  Quantity avalible hosts |     {net.num_addresses - 2}\n'
+    f'     Public               |     {isGlobal}\n'
     f'|-----------------------------------------------------------|\n'
 )
